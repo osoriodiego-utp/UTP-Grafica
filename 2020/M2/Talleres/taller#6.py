f40ec0b0544dc197c241eb046b50681939fc11a0
osoriodiego <dieferosorio@utp.edu.co>
2020-11-08 19:49:03 -0500

    F3 listo

--- 2020/M2/Talleres/taller#6.py
+++ 2020/M2/Talleres/taller#6.py
@@ -12,7 +12,7 @@ from color import *
 
 pygame.init()
 
-X, Y = 700, 700
+X, Y = 800, 800
 CX, CY = int(X/2), int(Y/2)
 
 ventana = pygame.display.set_mode((X, Y))
@@ -33,7 +33,61 @@ def draw_tower():
 
 # ==== (1.5) CUADROS1   ====
 def draw_squares_s():
-    pass
+    #Axes
+    pygame.draw.line(ventana, color.gris, (200, 400), (200, 800), 1)
+    pygame.draw.line(ventana, color.gris, (0, 600), (400, 600), 1)
+    #R1
+    pygame.draw.line(ventana, color.gris, (100, 400), (200, 500), 1)
+    pygame.draw.line(ventana, color.gris, (200, 500), (100, 600), 1)
+    pygame.draw.line(ventana, color.gris, (100, 600), (0, 500), 1)
+    pygame.draw.line(ventana, color.gris, (0, 500), (100, 400), 1)
+    pygame.draw.rect(ventana, color.gris, [50, 450, 100, 100], 1)
+    pygame.draw.line(ventana, color.gris, (100, 450), (150, 500), 1)
+    pygame.draw.line(ventana, color.gris, (150, 500), (100, 550), 1)
+    pygame.draw.line(ventana, color.gris, (100, 550), (50, 500), 1)
+    pygame.draw.line(ventana, color.gris, (50, 500), (100, 450), 1)
+    pygame.draw.line(ventana, color.gris, (75, 475), (125, 525), 1) 
+    pygame.draw.line(ventana, color.gris, (125, 475), (75, 525), 1) 
+
+    #R2
+    pygame.draw.line(ventana, color.gris, (100+200, 400), (200+200, 500), 1)
+    pygame.draw.line(ventana, color.gris, (200+200, 500), (100+200, 600), 1)
+    pygame.draw.line(ventana, color.gris, (100+200, 600), (0+200, 500), 1)
+    pygame.draw.line(ventana, color.gris, (0+200, 500), (100+200, 400), 1)
+    pygame.draw.rect(ventana, color.gris, [50+200, 450, 100, 100], 1)
+    pygame.draw.line(ventana, color.gris, (100+200, 450), (150+200, 500), 1)
+    pygame.draw.line(ventana, color.gris, (150+200, 500), (100+200, 550), 1)
+    pygame.draw.line(ventana, color.gris, (100+200, 550), (50+200, 500), 1)
+    pygame.draw.line(ventana, color.gris, (50+200, 500), (100+200, 450), 1)
+    pygame.draw.line(ventana, color.gris, (75+200, 475), (125+200, 525), 1) 
+    pygame.draw.line(ventana, color.gris, (125+200, 475), (75+200, 525), 1) 
+
+    #R3
+    pygame.draw.line(ventana, color.gris, (100, 400+200), (200, 500+200), 1)
+    pygame.draw.line(ventana, color.gris, (200, 500+200), (100, 600+200), 1)
+    pygame.draw.line(ventana, color.gris, (100, 600+200), (0, 500+200), 1)
+    pygame.draw.line(ventana, color.gris, (0, 500+200), (100, 400+200), 1)
+    pygame.draw.rect(ventana, color.gris, [50, 450+200, 100, 100+200], 1)
+    pygame.draw.line(ventana, color.gris, (100, 450+200), (150, 500+200), 1)
+    pygame.draw.line(ventana, color.gris, (150, 500+200), (100, 550+200), 1)
+    pygame.draw.line(ventana, color.gris, (100, 550+200), (50, 500+200), 1)
+    pygame.draw.line(ventana, color.gris, (50, 500+200), (100, 450+200), 1)
+    pygame.draw.line(ventana, color.gris, (75, 475+200), (125, 525+200), 1) 
+    pygame.draw.line(ventana, color.gris, (125, 475+200), (75, 525+200), 1)
+    
+    #R4
+    pygame.draw.line(ventana, color.gris, (100+200, 400+200), (200+200, 500+200), 1)
+    pygame.draw.line(ventana, color.gris, (200+200, 500+200), (100+200, 600+200), 1)
+    pygame.draw.line(ventana, color.gris, (100+200, 600+200), (0+200, 500+200), 1)
+    pygame.draw.line(ventana, color.gris, (0+200, 500+200), (100+200, 400+200), 1)
+    pygame.draw.rect(ventana, color.gris, [50+200, 450+200, 100, 100], 1)
+    pygame.draw.line(ventana, color.gris, (100+200, 450+200), (150+200, 500+200), 1)
+    pygame.draw.line(ventana, color.gris, (150+200, 500+200), (100+200, 550+200), 1)
+    pygame.draw.line(ventana, color.gris, (100+200, 550+200), (50+200, 500+200), 1)
+    pygame.draw.line(ventana, color.gris, (50+200, 500+200), (100+200, 450+200), 1)
+    pygame.draw.line(ventana, color.gris, (75+200, 475+200), (125+200, 525+200), 1) 
+    pygame.draw.line(ventana, color.gris, (125+200, 475+200), (75+200, 525+200), 1) 
+
 
 # ==== (1.5) CUADROS2   ====
 def draw_squares_c():
